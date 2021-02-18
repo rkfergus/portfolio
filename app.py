c25bf7c6ff55d400cd2cfb1463c92d043ea81a43
Rene Ferguson <reneferguson.dev@gmail.com>
2021-02-18 04:53:31 -0700

    remove unneccesary imports

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, send_file
-from flask_bootstrap import Bootstrap
 import plotly.express as px
-from pandas.api.types import CategoricalDtype
 
 app = Flask(__name__)
-Bootstrap(app)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
